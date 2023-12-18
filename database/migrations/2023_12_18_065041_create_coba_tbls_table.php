f5d6f573d4f40c33626746a3417fbc5e0faad111
Andreasaja <under.block22@gmail.com>
2023-12-18 16:18:50 +0700

    Initial commit

---
+++ database/migrations/2023_12_18_065041_create_coba_tbls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coba_tbls', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idx');
+            $table->string('nama');
+            $table->text('ket');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coba_tbls');
+    }
+};
